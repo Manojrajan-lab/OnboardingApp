c22b62bb5005808031799e755bcb11f1fdc7fd22
Manoj Rajan <manojrajan4934@gmail.com>
2026-02-15 11:56:31 +0530

    Onboarding Android App

---
+++ app/src/main/java/com/example/onboardingapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.onboardingapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Onboarding Completed 🚀", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
